9ffcdc659db9810c615192f021e51308e451a777
Ayush Rana <ayushrana200177@gmail.com>
2020-12-04 15:56:37 +0530

    version 1

---
+++ MyForm.h
@@ -0,0 +1,202 @@
+#pragma once
+
+namespace visualpasswordgenerator {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::TextBox^ textBox1;
+
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::NumericUpDown^ numericUpDown1;
+
+	protected:
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->numericUpDown1 = (gcnew System::Windows::Forms::NumericUpDown());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18.8F));
+			this->button1->Location = System::Drawing::Point(127, 272);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(316, 62);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Genrated";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.8F));
+			this->textBox1->Location = System::Drawing::Point(33, 200);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(510, 37);
+			this->textBox1->TabIndex = 1;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->BackColor = System::Drawing::Color::White;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.8F));
+			this->label1->ForeColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->label1->Location = System::Drawing::Point(102, 62);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(375, 31);
+			this->label1->TabIndex = 3;
+			this->label1->Text = L"Random password generation";
+			// 
+			// button2
+			// 
+			this->button2->BackColor = System::Drawing::Color::Transparent;
+			this->button2->FlatAppearance->BorderColor = System::Drawing::Color::White;
+			this->button2->FlatAppearance->MouseDownBackColor = System::Drawing::Color::White;
+			this->button2->FlatAppearance->MouseOverBackColor = System::Drawing::Color::White;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->ForeColor = System::Drawing::Color::Red;
+			this->button2->Location = System::Drawing::Point(12, 12);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(78, 35);
+			this->button2->TabIndex = 4;
+			this->button2->Text = L"EXIT";
+			this->button2->UseVisualStyleBackColor = false;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// numericUpDown1
+			// 
+			this->numericUpDown1->BackColor = System::Drawing::Color::Black;
+			this->numericUpDown1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.8F));
+			this->numericUpDown1->ForeColor = System::Drawing::Color::White;
+			this->numericUpDown1->Location = System::Drawing::Point(127, 132);
+			this->numericUpDown1->Maximum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 32, 0, 0, 0 });
+			this->numericUpDown1->Minimum = System::Decimal(gcnew cli::array< System::Int32 >(4) { 12, 0, 0, 0 });
+			this->numericUpDown1->Name = L"numericUpDown1";
+			this->numericUpDown1->ReadOnly = true;
+			this->numericUpDown1->Size = System::Drawing::Size(316, 37);
+			this->numericUpDown1->TabIndex = 5;
+			this->numericUpDown1->TextAlign = System::Windows::Forms::HorizontalAlignment::Right;
+			this->numericUpDown1->Value = System::Decimal(gcnew cli::array< System::Int32 >(4) { 12, 0, 0, 0 });
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->BackColor = System::Drawing::Color::White;
+			this->ClientSize = System::Drawing::Size(580, 380);
+			this->Controls->Add(this->numericUpDown1);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->button1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->numericUpDown1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+		//Gentrate password
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+		//Created vraidle
+		int count = 0;
+		String^ password;
+		int PassowrdLength = (int)numericUpDown1->Value;
+		Random^ randobj = gcnew Random;
+		while (count < PassowrdLength)
+		{
+			if (count == 0 || count == 4)
+			{
+				int randnum = randobj->Next(97, 122);
+				wchar_t NewChar = static_cast<wchar_t>(randnum);
+				password = password + NewChar;
+			}
+			else if (count==PassowrdLength-1|| count ==8)
+			{
+				int randnum = randobj->Next(65, 90);
+				wchar_t NewChar = static_cast<wchar_t>(randnum);
+				password = password + NewChar;
+			}
+			else if (count == 6)
+			{
+				int randnum = randobj->Next(49, 57);
+				wchar_t NewChar = static_cast<wchar_t>(randnum);
+				password = password + NewChar;
+			}
+			else if (count == 7)
+			{
+				int randnum = randobj->Next(64,64);
+				wchar_t NewChar = static_cast<wchar_t>(randnum);
+				password = password + NewChar;
+
+			}
+			else
+			{
+				int randnum = randobj->Next(33, 126);
+				wchar_t NewChar = static_cast<wchar_t>(randnum);
+				password = password + NewChar;
+			}
+			count = count + 1;
+		}
+		//Display the password
+		textBox1->Text = password;
+		
+	}
+		   //Exit button to exit the application;
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) {
+		Application::Exit();
+	}
+private: System::Void numericUpDown1_ValueChanged(System::Object^ sender, System::EventArgs^ e) {
+}
+};
+}
